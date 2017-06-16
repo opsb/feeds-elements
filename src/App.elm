7d498bdda56e4014896b3964903f7b371ba912dd
opsb <oliver@opsb.co.uk>
2017-06-16 23:51:02 +0200

    Sidenav

--- src/App.elm
+++ src/App.elm
@@ -1,10 +1,9 @@
 module App exposing (..)
 
-import Color exposing (rgba)
+import Color exposing (Color, rgba)
+import Color.Convert
 import Element exposing (..)
 import Element.Attributes exposing (..)
-import Html
-import Html.Attributes as Html
 import Style exposing (..)
 import Style.Border as Border
 import Style.Color as Color
@@ -17,7 +16,10 @@ type Styles
     | Container
     | Label
     | Navbar
-    | Sidebar
+    | NavLink
+    | SideBar
+    | SideBarTitle
+    | Nav
     | Inspector
     | MessageBox
     | Chat
@@ -25,8 +27,26 @@ type Styles
     | H3
 
 
+type Variations
+    = Active
+
+
+hex : String -> Color
+hex hexValue =
+    case Color.Convert.hexToColor hexValue of
+        Ok color ->
+            color
+
+        Err error ->
+            Debug.crash error
+
+
 colors =
-    { mauve = rgba 176 161 186 1
+    { darken = rgba 0 0 0 0.25
+    , mediumGrey = hex "#424242"
+    , lightGrey = rgba 158 158 158 1
+    , red = rgba 232 90 82 1
+    , mauve = rgba 176 161 186 1
     , blue1 = rgba 165 181 191 1
     , blue2 = rgba 171 200 199 1
     , green1 = rgba 184 226 200 1
@@ -34,10 +54,11 @@ colors =
     }
 
 
-stylesheet : StyleSheet Styles variation
+stylesheet : StyleSheet Styles Variations
 stylesheet =
     Style.stylesheet
         [ style None []
+        , style Main [ Font.typeface [ "Lato", "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ] ]
         , style Container
             [ Color.text Color.black
             , Color.background colors.blue2
@@ -49,16 +70,34 @@ stylesheet =
         , style Navbar
             [ Color.background colors.mauve
             ]
-        , style Sidebar
-            [ Color.background colors.green1
+        , style SideBar
+            [ Color.background colors.mediumGrey
+            ]
+        , style NavLink
+            [ Color.text colors.lightGrey
+            , Font.size 14
+            , hover
+                [ Color.background colors.darken
+                , Border.rounded 5
+                , Color.text Color.white
+                , cursor "pointer"
+                ]
+            , variation Active
+                [ Color.background colors.red
+                , Color.text Color.white
+                , Border.rounded 5
+                ]
+            ]
+        , style SideBarTitle
+            [ Color.text Color.white
+            , Font.uppercase
+            , Font.size 12
             ]
         , style Inspector
             [ Color.background colors.green2
             ]
         , style MessageBox
             [ Color.background colors.blue2 ]
-        , style Main
-            [ Font.typeface [ "helvetica" ] ]
         , style H3
             [ Font.size 20, Font.weight 400 ]
         ]
@@ -73,7 +112,7 @@ main =
                 [ height <| percent 100
                 , width <| fill 1
                 ]
-                [ sidebar, body, inspector ]
+                [ sideBar, body, inspector ]
             ]
 
 
@@ -81,13 +120,21 @@ navbar =
     el Navbar [ padding 20 ] (text "Navbar")
 
 
-sidebar =
-    column Sidebar
-        [ padding 20
+sideBar =
+    column SideBar
+        [ paddingXY 10 15
         , alignLeft
-        , width <| px 300
+        , width <| px 250
         ]
-        [ el H3 [] (text "Channels") ]
+        [ el SideBarTitle [ paddingXY 20 10 ] (text "Topics")
+        , column Nav
+            [ paddingXY 0 10, width <| fill 1 ]
+            [ el NavLink [ paddingXY 20 10 ] (text "One")
+            , el NavLink [ vary Active True, paddingXY 20 10 ] (text "Two")
+            , el NavLink [ paddingXY 20 10 ] (text "Three")
+            , el NavLink [ paddingXY 20 10 ] (text "Four")
+            ]
+        ]
 
 
 inspector =
